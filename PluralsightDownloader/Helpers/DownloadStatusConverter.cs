7c1ed9f9f613953ce4b102b0bca16257f4ef043c
Naveen <spk2naveen@hotmail.com>
2019-03-24 02:19:12 +0530

    Downloader

---
+++ PluralsightDownloader/Helpers/DownloadStatusConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace PluralsightDownloader.Helpers
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    public class DownloadStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var downloaded = (bool)value;
+            return downloaded ? "CheckboxMarkedCircleOutline" : "CheckboxBlankCircleOutline";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
